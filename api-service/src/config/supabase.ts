5eddee0e5235e91caccc4bd35e1877e27a60b1ec
Lê Bảo Hồng Hạnh <lebaohonghanh84@gmail.com>
2026-03-25 01:01:59 +0700

    update config file

--- api-service/src/config/supabase.ts
+++ api-service/src/config/supabase.ts
@@ -1,18 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_KEY!;
 
-if (!supabaseUrl || !supabaseServiceRoleKey) {
-  throw new Error(
-    'Missing SUPABASE_URL or SUPABASE_SERVICE_ROLE_KEY configuration',
-  );
-}
-
-export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey, {
-  auth: {
-    persistSession: false,
-    autoRefreshToken: false,
-    detectSessionInUrl: false,
-  },
-});
+export const supabase = createClient(supabaseUrl, supabaseKey);
